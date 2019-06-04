278ab8240b816fec268676a3ae9c82021c1b84e1
Alex Piet <alex.piet@OSXY0JHD2.local>
2019-06-04 16:44:02 -0700

    adding extract_data()

--- fit_tools.py
+++ fit_tools.py
@@ -456,5 +456,51 @@ def linear_change_flash(change_flash_params, change_flash_duration, change_flash
     return base
 
 
+def extract_data(data,dt):
+    licks = data['lick_timestamps']
+    running_timestamps = data['running_timestamps']
+    running_speed = data['running_speed']
+    rewards = np.round(data['reward_timestamps'],2)
+    flashes=np.round(data['stim_on_timestamps'],2)
+    rewardsdt = np.round(rewards*(1/dt))
+    flashesdt = np.round(flashes*(1/dt))
+    
+    stims = data['stim_id']
+    stims[np.array(stims) == 8 ] = 100
+    diffs = np.diff(stims)
+    diffs[(diffs > 50) | (diffs < -50 )] = 0
+    diffs[ np.abs(diffs) > 0] = 1
+    diffs = np.concatenate([[0], diffs])
+    
+    change_flashes = flashes[diffs == 1]
+    change_flashesdt = np.round(change_flashes*(1/dt))
+    # get start/stop time for session
+    start_time = 1
+    stop_time = int(np.round(running_timestamps[-1],2)*(1/dt))
+    licks = licks[licks < stop_time/100]
+    licks = np.round(licks,2)
+    licksdt = np.round(licks*(1/dt))
+    time_vec = np.arange(0,stop_time/100.0,dt)
+    return licks, licksdt, start_time, stop_time, time_vec, running_spped, rewardsdt, flashesdt, change_flashesdt
+    
 
+def extract_sdk_data(data,dt):
+    licks = data['lick_timestamps']
+    running_timestamps = data['running_timestamps']
+    running_speed = data['running_speed']
+    rewards = np.round(data['reward_timestamps'],2)
+    flashes=np.round(data['stim_on_timestamps'],2)
+    rewardsdt = np.round(rewards*(1/dt))
+    flashesdt = np.round(flashes*(1/dt))
+    change_flashes = np.round(data["stim_change_time"],2)
+    change_flashesdt = np.round(change_flashes*(1/dt))
+    # get start/stop time for session
+    start_time = 1
+    stop_time = int(np.round(running_timestamps[-1],2)*(1/dt))
+    licks = licks[licks < stop_time/100]
+    licks = np.round(licks,2)
+    licksdt = np.round(licks*(1/dt))
+    time_vec = np.arange(0,stop_time/100.0,dt)
+    return licks, licksdt, start_time, stop_time, time_vec, running_spped, rewardsdt, flashesdt, change_flashesdt
+    
 
