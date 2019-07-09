bcbee165c510e11fc0f036309b32b2326d348a2d
Alex Piet <alexpiet@gmail.com>
2019-07-09 16:51:51 -0700

    gearing up for fitting over learning

--- src/psy_dev.py
+++ src/psy_dev.py
@@ -14,11 +14,16 @@ ps.plot_session_summary(stages[4]+stages[6],savefig=True,group_label="B_")
 
 # TODO
 # Next round of fits should: 
+    # Fit every session in nick's database. Including the behavior only sessions which might need to be processed separately
     # put stage_name into metadata
     # save results as a dictionary rather than a list
+    # finalize the dropout features
     # do clustering
-    # 
 # 0. fit for all ophys sessions for one mouse 
+    # Filter the relevant sessions
+    # Make a function that formats just one session (for behavior sessions)
+    # Make a function that stiches sessions together 
+    # set up fitting infrastructure
 # 1. fits over learning
 # 2. document psy_tools()
 # 3. make list of on-going issues to tackle later
@@ -50,5 +55,39 @@ behavior_psydata = ps.format_all_sessions(all_flash_df)
 hyp2, evd2, wMode2, hess2, credibleInt2,weights2 = ps.fit_weights(behavior_psydata,TIMING4=True)
 ypred2 = ps.compute_ypred(behavior_psydata, wMode2,weights2)
 ps.plot_weights(session,wMode2, weights2,behavior_psydata,errorbar=credibleInt2, ypred = ypred2,validation=False,session_labels = behavior_sessions.stage_name.values)
+
+
+
+
+from allensdk.brain_observatory.behavior import behavior_ophys_session as bos
+from allensdk.brain_observatory.behavior import behavior_session as bs
+from allensdk.brain_observatory.behavior import stimulus_processing
+from allensdk.internal.api import behavior_ophys_api as boa
+from allensdk.internal.api import behavior_lims_api as bla
+
+# df with all the ophys and behavior sessions
+vb_sessions = pd.read_hdf('/home/nick.ponvert/nco_home/data/vb_sessions.h5', key='df')
+mice = np.unique(vb_sessions['donor_id'])
+vb_sessions[vb_sessions['donor_id'] == mice[7]]
+
+def load_session(row):
+    if pd.isnull(row['ophys_experiment_id']):
+        api = bla.BehaviorLimsApi(int(row['behavior_session_id']))
+        session_id = 'behavior_{}'.format(int(row['behavior_session_id']))
+        session = bs.BehaviorSession(api)
+    else:
+        api = boa.BehaviorOphysLimsApi(int(row['ophys_experiment_id']))
+        session_id = 'ophys_{}'.format(int(row['ophys_experiment_id']))
+        session = bos.BehaviorOphysSession(api)
+    return session, session_id
+
+
+
+
+
+
+
+
+
 
 
