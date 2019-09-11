b2a54573a0dcd42b23078ba193f5795be7115136
Alex Piet <alexpiet@gmail.com>
2019-09-10 17:23:22 -0700

    adding tools for histograms of licking times

--- src/psy_timing_dev.py
+++ src/psy_timing_dev.py
@@ -1,8 +1,8 @@
 import psy_tools as ps
 from alex_utils import *
 import numpy as np
+import psy_timing_tools as pt
 
-# save out 2x2 grid of blue/orange plots for each mouse
 # calculate blue/orange ratio for each session
 # Calculate post-ili, and pre-ili
 # label licking bouts, and give them all the "rewarded" property
@@ -12,7 +12,51 @@ import numpy as np
 session_ids = ps.get_session_ids()
 mice_ids = ps.get_mice_ids()
 session = ps.get_data(session_ids[0])
-
 sessions,IDS, active =ps.load_mouse(mice_ids[0])
+
+## Plot Summary
+all_durs = pt.get_all_mouse_durations(mice_ids)
+pt.plot_all_mouse_durations(all_durs)
+
+# Plot Distribution plots
+mice_ids = ps.get_mice_ids()
+directory = '/home/alex.piet/codebase/behavior/psy_fits_v2/'
+for mouse in mice_ids:
+    print(mouse)
+    try:
+        pt.plot_mouse_lick_distributions(mouse,directory=directory)
+    except:
+        print(" crash")
+
+total = []
+hits = []
+for id in session_ids:
+    print(id)
+    try:
+        this_total,this_hit = pt.get_lick_count(id)
+        total.append(this_total)
+        hits.append(this_hit)
+    except:
+        print(" crash")
+
+plt.figure()
+plt.plot(total,hits,'ko')
+plt.ylabel('# Hits',fontsize=12)
+plt.xlabel('# Non-bout Licks',fontsize=12)
+
+
+
+for id in session_ids:
+    print(id)
+    try:
+        pt.plot_lick_distribution(ps.get_data(id),directory=directory)
+    except:
+        print(" crash")
+
+
+
+
+
+
 
 
--- src/psy_timing_tools.py
+++ src/psy_timing_tools.py
@@ -4,24 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-def annotate_licks(session):
-    # ili
-    # rewarded
-    # consumption
-    licks = session.licks
-    licks['pre-ili'] = np.concatenate([[np.nan],np.diff(licks.timestamps.values)])
-    licks['post-ili'] = np.concatenate([np.diff(licks.timestamps.values),[np.nan]])
-    licks['rewarded'] = False
-    for index, row in session.rewards.iterrows():
-        mylick = np.where(licks.timestamps <= row.timestamps)[0][-1]
-        licks.at[mylick,'rewarded'] = True
-
 def plot_all_mouse_durations(all_durs):
     plt.figure()
     for dur in all_durs:
         if len(dur) == 4:
-            plt.plot(dur)
+            plt.plot(dur,'o-')
+    plt.ylabel('Avg ILI (s)')
+    plt.xlabel('Ophys Session #')
+    plt.ylim(bottom=0)
 
 def get_all_mouse_durations(mice_ids):
     all_durs=[]
@@ -38,25 +28,85 @@ def get_mouse_durations(mouse_id):
         durs.append(get_mean_lick_distribution(sess))
     return durs
 
-# Make Figure of distribution of licks
-def plot_lick_distribution(session):
+
+def get_lick_count(id):
+    session = ps.get_data(id)
+    annotate_licks(session)
+    d = session.licks['pre-ili'][session.licks.rewarded]
+    hits = len(d[(d>.7)& (d<10)].values)
+    d = session.licks['pre-ili']
+    total = len(d[(d>.7)& (d<10)].values)   
+    return total, hits
+
+def annotate_licks(session):
+    # ili
+    # rewarded
+    # consumption
+    licks = session.licks
+    licks['pre-ili'] = np.concatenate([[np.nan],np.diff(licks.timestamps.values)])
+    licks['post-ili'] = np.concatenate([np.diff(licks.timestamps.values),[np.nan]])
+    licks['rewarded'] = False
+    for index, row in session.rewards.iterrows():
+        mylick = np.where(licks.timestamps <= row.timestamps)[0][-1]
+        licks.at[mylick,'rewarded'] = True
+
+def plot_mouse_lick_distributions(id,nbins=50,directory=None):
+    sessions, ids, active = ps.load_mouse(id)
+    sessions = np.array(sessions)[np.array(active)]
+    fig,ax = plt.subplots(2,2)
+    if len(sessions) > 0:
+        plot_mouse_lick_distributions_inner(sessions[0], ax[0,0],nbins,id)
+    if len(sessions) > 1:
+        plot_mouse_lick_distributions_inner(sessions[1], ax[0,1],nbins,id)
+    if len(sessions) > 2:
+        plot_mouse_lick_distributions_inner(sessions[2], ax[1,0],nbins,id)
+    if len(sessions) > 3:
+        plot_mouse_lick_distributions_inner(sessions[3], ax[1,1],nbins,id)
+    plt.tight_layout()
+    if type(directory) is not type(None):
+        plt.savefig(directory+"mouse_"+str(id)+"_ILI.png")
+
+def plot_mouse_lick_distributions_inner(session, ax,nbins,id):
     annotate_licks(session)
     licks = session.licks.timestamps.values
     diffs = np.diff(licks)
-    plt.figure()
-    plt.hist(diffs[diffs<10],50,label='All')
-    ax = plt.gca()
+    h=ax.hist(diffs[diffs<10],nbins,label='All')
     ax.axvline(0.75,linestyle='--',color='k')
-    plt.ylabel('count')
-    plt.xlabel('InterLick (s)')
-    plt.ylim([0,100])
-    plt.title(str(session.metadata['mouse_id'])+" "+session.metadata['stage'])
+    ax.set_ylabel('count')
+    ax.set_xlabel('InterLick (s)')
+    ax.set_ylim([0,100])
+    ax.set_title(str(id)+" "+session.metadata['stage'])
     m = get_mean_lick_distribution(session)
     ax.axvline(m,linestyle='--',color='r')
     d = session.licks['pre-ili'][session.licks.rewarded]
-    plt.hist(d[(d>.7)&(d<10)],30,label='Hits')
-    plt.legend()
+    h2= ax.hist(d[(d>.7)&(d<10)],bins=h[1],label='Hits')
 
+# Make Figure of distribution of licks
+def plot_lick_distribution(session,nbins=50,directory=None):
+    annotate_licks(session)
+    licks = session.licks.timestamps.values
+    diffs = np.diff(licks) 
+    fig, ax = plt.subplots(1,2,figsize=(12,5))
+    h=ax[0].hist(diffs[diffs<10],nbins,label='All')
+    ax[0].axvline(0.75,linestyle='--',color='k')
+    ax[0].set_ylabel('count')
+    ax[0].set_xlabel('InterLick (s)')
+    ax[0].set_ylim([0,100])
+    ax[0].set_title(str(session.metadata['mouse_id'])+" "+session.metadata['stage'])
+    m = get_mean_lick_distribution(session)
+    ax[0].axvline(m,linestyle='--',color='r')
+    d = session.licks['pre-ili'][session.licks.rewarded]
+    h2= ax[0].hist(d[(d>.7)&(d<10)],bins=h[1],label='Hits')
+    ax[0].legend()
+    h1 = np.histogram(diffs[(diffs>.7)&(diffs<10)],bins=h[1])
+    centers = np.diff(h[1]) + h[1][0:-1]
+    ax[1].plot(centers[centers > .7], (h1[0]-h2[0])[centers > .7]  ,'k-')
+    ax[1].set_ylabel('Miss Licks')
+    ax[1].set_xlabel('InterLick (s)')
+    if type(directory) is not type(None):
+        id = session.metadata['ophys_experiment_id']
+        plt.savefig(directory+str(id)+"_ILI.png")
+ 
 def get_mean_lick_distribution(session):
     licks = session.licks.timestamps.values
     diffs = np.diff(licks)
