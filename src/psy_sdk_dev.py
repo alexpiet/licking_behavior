6e75e587a8de1d714c88c622bfa9aa835757133c
Alex Piet <alexpiet@gmail.com>
2019-09-19 14:34:32 -0700

    minor

--- src/psy_sdk_dev.py
+++ src/psy_sdk_dev.py
@@ -21,22 +21,12 @@ psd.latency_behavior_by_cluster(cdf,'4',session=id,stage = stage)
 plt.figure(); plt.plot(fit['all_clusters']['4'][1])
 
 # for many sessions is slow and takes a lot of memory
+# Need to also parse by cell type
 cache = ps.get_cache()
 manifest = ps.get_manifest()
 sessions, fits, cdfs = psd.build_multi_session_joint_table(session_ids[0:40],cache, manifest, use_all_clusters=True,slim_df=True)
 psd.mean_response_by_cluster(cdfs,'4',session=session_ids[0:40],stage = "")
 psd.running_behavior_by_cluster(cdfs,'4',session=session_ids[0:40],stage = "")
 psd.latency_behavior_by_cluster(cdfs,'4',session=session_ids[0:40],stage = stage)
-
-
-# one off
-for id in session_ids:
-    print(id)
-    try:
-        psd.full_analysis(id)
-    except:
-        print(" crash")
-    plt.close('all')
-
 
 
