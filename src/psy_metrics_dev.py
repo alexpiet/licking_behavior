8b998088d75dc76baf82e492ae722ad3bff4cad6
Alex Piet <alexpiet@gmail.com>
2019-09-24 15:44:09 -0700

    adding some summary figures

--- src/psy_metrics_dev.py
+++ src/psy_metrics_dev.py
@@ -7,39 +7,42 @@ import pandas as pd
 plt.ion()
 
 id = ps.get_session_ids()[27] #30
-
 session = ps.get_data(id)
+
+# Get model clusters
 fit = ps.load_fit(session.ophys_experiment_id)
 fit = ps.plot_fit(session.ophys_experiment_id,cluster_labels=fit['all_clusters']['3'][1],num_clusters=3)
 fit = ps.plot_fit(session.ophys_experiment_id,cluster_labels=fit['cluster']['3'][1],num_clusters=3)
 
-
+# Plot rolling indices
 df = session.get_rolling_performance_df()
 trials = session.trials
 trials = pd.concat([trials,df],axis=1,sort=False)
-
-
-# Plot rolling indices
 plt.figure()
 plt.plot(trials.start_time, trials.reward_rate,'k',label='reward')
 plt.plot(trials.start_time, trials.rolling_dprime,'b',label='dprime')
 plt.plot(trials.start_time, trials.hit_rate,'r',label='hit')
 
-# make these plots for all sessions
+# make metric plots for all sessions
 for id in ps.get_session_ids():
     print(id)
     try:
         filename = '/home/alex.piet/codebase/behavior/psy_fits_v2/'+str(id)
         session = ps.get_data(id)
-        pt.annotate_licks(session)
-        pm.annotate_bouts(session)
-        pm.annotate_flash_rolling_metrics(session)
-        pm.classify_by_flash_metrics(session)
+        pm.get_metrics(session)
         pm.plot_metrics(session,filename=filename+'_metrics')
         pm.plot_2D(session,filename=filename+'_metrics_2D')
         plt.close('all')
     except:
         print(' crash')
+
+# get epoch across time in session
+all_epochs = pm.get_all_epochs()
+pm.plot_all_epochs(all_epochs)
+
+# get time in each epoch across all sessions
+times,count,all_times = pm.get_all_times()
+pm.plot_all_times(times,count,all_times)
 
 
 
--- src/psy_metrics_tools.py
+++ src/psy_metrics_tools.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import psy_timing_tools as pt
+import psy_tools as ps
 import seaborn as  sns
 import pandas as pd
 import matplotlib.patches as patches
@@ -7,6 +9,21 @@ import matplotlib.patches as patches
 def get_metrics(session):
     '''
         Top level function that appeneds a few columns to session.stimulus_presentations 
+
+        ARGUMENTS: session, SDK session object
+
+        Adds to the stimulus_presentations table
+            bout_start, (boolean)
+            licked, (boolean)
+            lick_rate, licks/flash
+            rewarded, (boolean)
+            reward_rate, rewards/flash
+            running_rate
+            bout_rate, bouts/flash
+            high_lick, (boolean)
+            high_reward, (boolean)
+            flash_metrics_epochs, (int)
+            flash_metrics_labels, (string)
     '''
     pt.annotate_licks(session)
     annotate_bouts(session)
@@ -14,6 +31,9 @@ def get_metrics(session):
     classify_by_flash_metrics(session)
 
 def annotate_bouts(session):
+    '''
+        Uses the bout annotations in session.licks to annotate session.stimulus_presentations
+    '''
     bout_starts = session.licks[session.licks['bout_start']]
     session.stimulus_presentations['bout_start'] = False
     for index,x in bout_starts.iterrows():
@@ -23,6 +43,9 @@ def annotate_bouts(session):
     session.stimulus_presentations.drop(-1,inplace=True)
 
 def annotate_flash_rolling_metrics(session,win_dur=320, win_type='triang'):
+    '''
+        Get rolling flash level metrics for lick rate, reward rate, and bout_rate
+    '''
     # Get Lick Rate / second
     session.stimulus_presentations['licked'] = [1 if len(this_lick) > 0 else 0 for this_lick in session.stimulus_presentations['licks']]
     session.stimulus_presentations['lick_rate'] = session.stimulus_presentations['licked'].rolling(win_dur, min_periods=1,win_type=win_type).mean()/.75
@@ -38,6 +61,9 @@ def annotate_flash_rolling_metrics(session,win_dur=320, win_type='triang'):
     session.stimulus_presentations['bout_rate'] = session.stimulus_presentations['bout_start'].rolling(win_dur,min_periods=1, win_type=win_type).mean()/.75
 
 def classify_by_flash_metrics(session, lick_threshold = 0.1, reward_threshold=2/80,use_bouts=True):
+    '''
+        Use the flash level rolling metrics to classify into three states based on the thresholds
+    '''
     if use_bouts:
         session.stimulus_presentations['high_lick'] = [True if x > lick_threshold else False for x in session.stimulus_presentations['bout_rate']] 
     else:
@@ -47,6 +73,9 @@ def classify_by_flash_metrics(session, lick_threshold = 0.1, reward_threshold=2/
     session.stimulus_presentations['flash_metrics_labels'] = ['low-lick,low-reward' if x==0  else 'high-lick,high-reward' if x==1 else 'high-lick,low-reward' for x in session.stimulus_presentations['flash_metrics_epochs']]
 
 def plot_metrics(session,use_bouts=True,filename=None):
+    '''
+        plot the lick and reward rates for this session with the classified epochs
+    '''
     plt.figure(figsize=(11,4))
     if 'bout_rate' not in session.stimulus_presentations:
         annotate_flash_rolling_metrics(session)
@@ -57,7 +86,7 @@ def plot_metrics(session,use_bouts=True,filename=None):
     cp = np.where(~(np.diff(cluster_labels) == 0))[0]
     cp = np.concatenate([[0], cp, [len(cluster_labels)]])
     plotted = np.zeros(3,)
-    labels = ['low-lick,low-reward','high-lick,high-reward','high-lick,low-reward']
+    labels = ['low-lick, low-reward','high-lick, high-reward','high-lick, low-reward']
     for i in range(0, len(cp)-1):
         if plotted[cluster_labels[cp[i]+1]]:
             plt.gca().axvspan(cp[i],cp[i+1],color=cluster_colors[cluster_labels[cp[i]+1]], alpha=0.2)
@@ -65,14 +94,13 @@ def plot_metrics(session,use_bouts=True,filename=None):
             plotted[cluster_labels[cp[i]+1]] = True
             plt.gca().axvspan(cp[i],cp[i+1],color=cluster_colors[cluster_labels[cp[i]+1]], alpha=0.2,label=labels[cluster_labels[cp[i]+1]])
 
-    plt.plot(session.stimulus_presentations.reward_rate,'m',label='Flash Reward')
+    plt.plot(session.stimulus_presentations.reward_rate,'m',label='Reward Rate')
+    plt.gca().axhline(2/80,linestyle='--',alpha=0.5,color='m',label='Reward Threshold')
     if use_bouts:
-        plt.plot(session.stimulus_presentations.bout_rate,'g',label='Flash Lick')
+        plt.plot(session.stimulus_presentations.bout_rate,'g',label='Lick Rate')
     else:
         plt.plot(session.stimulus_presentations.lick_rate,'g',label='Flash Lick')
-    plt.gca().axhline(0,linestyle='--',alpha=0.5,color='k')
-    plt.gca().axhline(2/80,linestyle='--',alpha=0.5,color='m')
-    plt.gca().axhline(.1,linestyle='--',alpha=0.5,color='g')
+    plt.gca().axhline(.1,linestyle='--',alpha=0.5,color='g',label='Lick Threshold')
     plt.xlabel('Flash #',fontsize=12)
     plt.ylabel('Rate/Flash',fontsize=12)
     plt.legend()
@@ -103,10 +131,81 @@ def plot_2D(session,lick_threshold = 0.1, reward_threshold = 2/80,filename=None)
     if type(filename) is not type(None):
         plt.savefig(filename+".png")
     
+def get_time_in_epochs(session):
+    x0 = np.sum(session.stimulus_presentations.flash_metrics_epochs == 0) 
+    x1 = np.sum(session.stimulus_presentations.flash_metrics_epochs == 1)
+    x2 = np.sum(session.stimulus_presentations.flash_metrics_epochs == 2) 
+    times = np.array([x0,x1,x2])*0.75    
+    return times
+
+def get_all_times():
+    times = np.zeros(3,)
+    count = 0
+    all_times = []
+    for id in ps.get_session_ids():
+        print(id)
+        try:
+            session = ps.get_data(id)
+            get_metrics(session)
+            my_times = get_time_in_epochs(session)
+            times += my_times
+            count +=1
+            all_times.append(my_times)
+        except:
+            print(' crash')
+    all_times = np.vstack(all_times)
+    return times, count,all_times
+
+def plot_all_times(times,count,all_times):
+    plt.figure(figsize=(4,8))
+    labels = ['low-lick\nlow-reward','high-lick\nhigh-reward','high-lick\nlow-reward']
+    means = np.mean(all_times/np.sum(all_times,1)[:,None],0)*100
+    sem = np.std(all_times/np.sum(all_times,1)[:,None],0)/np.sqrt(53)*100
+    colors = sns.color_palette("hls",3)   
+    for i in range(0,3):
+        plt.plot([i-.5,i+.5],[means[i],means[i]],'-',color=colors[i],linewidth=4)
+        plt.plot([i,i], [means[i]-sem[i], means[i]+sem[i]], 'k-')
+    plt.xticks([0,1,2],labels,rotation=90,fontsize=12)
+    plt.ylabel('% of time in each epoch',fontsize=12)
+    plt.ylim([0,100])
+    plt.tight_layout()
+
+def get_all_epochs():
+    epochs = []
+    for id in ps.get_session_ids():
+        print(id)
+        try:
+            session = ps.get_data(id)
+            get_metrics(session)
+            my_epochs = session.stimulus_presentations['flash_metrics_epochs'].values
+            epochs.append(my_epochs)
+        except:
+            print(' crash')
     
+    lens = [len(x) for x in epochs]
+    all_epochs = np.zeros((len(epochs), np.max(lens)))
+    all_epochs[:] = np.nan
+    for i in range(0,len(epochs)):
+        all_epochs[i,0:len(epochs[i])] = epochs[i]
+    return all_epochs
+
+def plot_all_epochs(all_epochs):
+    plt.figure(figsize=(11,4))
+    colors = sns.color_palette("hls",3)   
+    labels = ['low-lick, low-reward','high-lick, high-reward','high-lick, low-reward']
+    for i in range(0,3):
+        plt.plot(np.sum(all_epochs==i,0)/53*100,color=colors[i],label=labels[i])
+    
+    plt.ylim([0,100])
+    plt.xlim([0,4790])
+    plt.legend()
+    plt.ylabel('% of session in each epoch',fontsize=12)
+    plt.xlabel('Flash #',fontsize=12)
+    plt.tight_layout()
 
 
 
+    
 
 
 
