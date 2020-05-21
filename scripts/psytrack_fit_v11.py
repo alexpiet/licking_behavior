69bfc557a3ea1c423bea361a9f69890008e7c4ba
Alex Piet <alexpiet@gmail.com>
2020-05-21 15:19:02 -0700

    Setting up v11

---
+++ scripts/psytrack_fit_v11.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+import psy_tools as ps
+
+if __name__ == '__main__':
+    name_of_this_file   = sys.argv[0]
+    behavior_session_id       = sys.argv[1]
+    
+    dirc = "/home/alex.piet/codebase/behavior/psy_fits_v11/"
+    ps.process_session(behavior_session_id,directory=dirc,format_options={'mean_center':True},complete=False)
+
