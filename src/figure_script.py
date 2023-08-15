1949aee964c5fb9139cff0274e70e78b95fc8763
Alex Piet <alexpiet@gmail.com>
2023-08-15 13:23:06 -0700

    documenting running histograms

--- src/figure_script.py
+++ src/figure_script.py
@@ -217,6 +217,9 @@ def make_figure_2_novelty():
         pv.plot_session_summary_trajectory(summary_df,key,BEHAVIOR_VERSION,
             categories='experience_level',savefig=True,filetype='.svg',xaxis_images=False)
 
+def make_figure_2_running():
+    summary_df = po.get_ophys_summary_table(BEHAVIOR_VERSION)
+    pv.histogram_of_running_speeds(summary_df)
 
 def make_figure_3():
     summary_df = po.get_ophys_summary_table(BEHAVIOR_VERSION)
