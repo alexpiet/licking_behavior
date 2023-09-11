fixing error in engagement_v2 computation

src/figure_script.py
@@ -243,7 +243,7 @@ def make_figure_3():
 
 def make_figure_3_example():
     session = pgt.get_data(FIG3_BSID)
-    pv.plot_session_metrics(session, plot_list=['reward_rate'],
+    pv.plot_session_metrics(session, plot_list=['reward_rate','lick_bout_rate'],
         plot_engagement_example=True,version=BEHAVIOR_VERSION)
 
 def make_figure_3_engagement_supplement():
@@ -266,6 +266,13 @@ def make_figure_3_engagement_supplement():
 
     # compute KL divergence of RT distributions compared to uniform
     pv.RT_entropy(summary_df,savefig=True, version=BEHAVIOR_VERSION,nboots=1000)
+
+def disengagement_hits():
+    summary_df = po.get_ophys_summary_table(BEHAVIOR_VERSION)   
+    summary_df['num_hits_disengaged'] = [\
+        np.sum(summary_df.loc[x]['hit'][summary_df.loc[x]['engagement_v2']==False]==1) \
+        for x in summary_df.index.values]
+      
 
 def make_figure_4_supplement_strategy_matched():
     summary_df = po.get_ophys_summary_table(BEHAVIOR_VERSION)

src/psy_output_tools.py
@@ -179,7 +179,7 @@ def temporary_engagement_updates(summary_df):
     summary_df['engagement_v1'] = summary_df['engaged']
     v2 = []
     for index, row in tqdm(summary_df.iterrows(), total=summary_df.shape[0]):
-        this = np.array([x[0] and x[1] > 0.1 for x in zip(row.engagement_v1, row.lick_bout_rate)])
+        this = np.array([x[0] or (x[1] > 0.1) for x in zip(row.engagement_v1, row.lick_bout_rate)])
         v2.append(this)
     summary_df['engagement_v2'] = v2
     return summary_df

src/psy_visualization.py
@@ -2302,7 +2302,7 @@ def plot_session_metrics(session, plot_list = ['reward_rate','lick_hit_fraction'
 
     # Plot Engagement state
     if not plot_example:
-        engagement_labels = session.stimulus_presentations['engaged'].values
+        engagement_labels = session.stimulus_presentations['engaged_v2'].values
         engagement_labels=[0 if x else 1 for x in engagement_labels]
         change_point = np.where(~(np.diff(engagement_labels) == 0))[0]
         change_point = np.concatenate([[0], change_point, [len(engagement_labels)]])